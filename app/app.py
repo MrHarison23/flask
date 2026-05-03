a109166f38c10167b442b04dbb27b9d874fe5efb
MrHarison23 <starkharison@gmail.com>
2026-05-03 20:28:50 +0300

    Fix: ssh/config

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
 
-app - Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def home():
-    return "Version 1 🚀"
+    return "Финальная победа! Все работает!"
 
-app/run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
